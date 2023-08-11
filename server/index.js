f96cec9a6c7e4598d29dc40b4d5b0194c7894a0a
Festus <festusgitahik@gmail.com>
2023-08-11 12:52:04 +0300

    worked on database schema

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// ! import files
+const {  users, category, expense} = require('./schemas/userSchema')
 
-// ! connection to mongodb
-const connectionString = 'mongodb://127.0.0.1:27017/payment'
-const connect = () =>{
-    
-}
 
-app.listen(8000, ()=>{
-    console.log("listening on port 8000");
+// ! connection to mongodb and server
+const connect = mongoose.connect('mongodb://127.0.0.1:27017/payment').then(()=>{
+    app.listen(8000, ()=>{
+        console.log("listening on port 8000");
+    })
 })
+
+
+// ! app routes
+
+
 
--- server/schemas/userSchema.js
+++ server/schemas/userSchema.js
@@ -1,1 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+  },
+  firstName: String,
+  profilePic: String,
+  passwordHash: String,
+//   ! user to share costs with
+  sharingUser: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "Users",
+  },
+});
+
+const categorySchema = new mongoose.Schema({
+  Category: String,
+});
+
+const expenseSchema = new mongoose.Schema({
+  item: String,
+  Cost: Number,
+  DateAdded: {
+    type: Date,
+    default: Date.now(),
+    immutable: true,
+  },
+
+  boughtBy: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "Users",
+  },
+  category: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "Category",
+  },
+});
+
+const users = mongoose.model("Users", userSchema);
+const category = mongoose.model("Category", categorySchema);
+const expense = mongoose.model("Expense", expenseSchema);
+
+module.exports = { users, category, expense };
